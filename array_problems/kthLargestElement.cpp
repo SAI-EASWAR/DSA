2cde2594c5318f0d120b955a1d7b436fef737111
e21cseu0252@bennett.edu.in <113098477+SAI-EASWAR@users.noreply.github.com>
2023-11-06 22:14:29 +0530

    6 nov 2023

---
+++ array_problems/kthLargestElement.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <queue>
+
+class Solution {
+public:
+    int findKthLargest(std::vector<int>& nums, int k) {
+        int N = nums.size();
+        std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
+        
+        for (int i = 0; i < N; i++) {
+        // Push the current element onto the max heap
+        pq.push(nums[i]);
+ 
+        // If the size of the max heap exceeds K, remove the largest element
+        if (pq.size() > k)
+            pq.pop();
+    }    return pq.top(
+        
+    );
+    }
+};
